6b473218ceb6f435a59800a99f25419c2c701a17
BUGMAN-ini <bshaverda@gmail.com>
2024-06-23 16:22:36 +0400

    Added HttpClientFactory

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,81 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDTO?> SendAsync(RequestDTO request)
+        {
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            var message = new HttpRequestMessage();
+            message.Headers.Add("Accept", "application/json");
+            //Token
+
+            message.RequestUri = new Uri(request.Url);
+            if(request.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(request.Data), Encoding.UTF8, "application/json");
+            }
+
+            HttpResponseMessage? response = null;
+
+            switch(request.ApiType)
+            {
+                case ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+            response = await client.SendAsync(message);
+
+            try
+            {
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    default:
+                        var apicontent = await response.Content.ReadAsStringAsync();
+                        var apiresponsedto = JsonConvert.DeserializeObject<ResponseDTO>(apicontent);
+                        return apiresponsedto;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDTO
+                {
+                    Message = ex.Message,
+                    IsSuccess = false,
+                };
+                return dto;
+            }
+        }
+    }
+}
