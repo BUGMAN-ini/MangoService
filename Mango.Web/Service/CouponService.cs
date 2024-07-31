39fc127b98514de861fac938b4f39d8639a2323b
beqa shaverdashvili <bshaverda@gmail.com>
2024-08-01 00:24:47 +0400

    Added HttpClient Services and Configured Program cs for it

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseservice;
+
+        public CouponService(IBaseService baseservice)
+        {
+            _baseservice = baseservice;
+        }
+
+        public Task<ResponseDTO?> CreateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponAsync(string couponId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponByIdAsync(int couponId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> UpdateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDTO?> GetAllCouponAsync();
+        Task<ResponseDTO?> GetCouponAsync(string couponId);
+        Task<ResponseDTO?> GetCouponByIdAsync(int couponId);
+        Task<ResponseDTO?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDTO?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDTO?> DeleteCouponAsync(int id); 
+        
+    }
+}
