e8cbd373df50849a27f739690704d127f425650f
BUGMAN-ini <bshaverda@gmail.com>
2024-08-04 19:06:20 +0400

    added action methods in front

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -24,5 +24,53 @@ namespace Mango.Web.Controllers
             }
             return View(coupons);
         }
-    }
+
+        public async Task<IActionResult> CreateCoupon()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+		public async Task<IActionResult> CreateCoupon(CouponDto coupon)
+		{
+            if(ModelState.IsValid)
+            {
+                ResponseDTO? response = await _couponservice.CreateCouponAsync(coupon);
+                if(response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+
+			return View(coupon);
+		}
+
+
+		public async Task<IActionResult> CouponDelete(int CouponId)
+		{
+			List<CouponDto>? coupons = new();
+
+			ResponseDTO? response = await _couponservice.GetCouponByIdAsync(CouponId);
+			if (response != null && response.IsSuccess)
+			{
+				CouponDto? model = JsonConvert.DeserializeObject<CouponDto?>(Convert.ToString(response.Result));
+                return View(model);
+			}
+			return NotFound();
+		}
+
+        [HttpPost]
+		public async Task<IActionResult> CouponDelete(CouponDto CouponDTO)
+		{
+
+			ResponseDTO? response = await _couponservice.DeleteCouponAsync(CouponDTO.CouponId);
+
+			if (response != null && response.IsSuccess)
+			{
+                return RedirectToAction(nameof(CouponIndex));
+			}
+			return View(CouponDTO);
+		}
+	}
 }
