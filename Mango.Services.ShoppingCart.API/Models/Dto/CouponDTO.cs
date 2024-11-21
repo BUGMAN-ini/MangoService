66e8d525227de71528ede3d42d82145347c64e23
BUGMAN-ini <bshaverda@gmail.com>
2024-11-21 10:26:40 +0400

    added shopping cart service

---
+++ Mango.Services.ShoppingCart.API/Models/Dto/CouponDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ShoppingCart.API.Models.Dto
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public int DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
