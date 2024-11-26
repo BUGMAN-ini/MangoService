371670426f2d6b5cac83643657c53321e56ad65f
BUGMAN-ini <bshaverda@gmail.com>
2024-11-26 22:13:01 +0400

    Added Gateway Service

---
+++ Mango.GatewaySolution/Program.cs
@@ -0,0 +1,17 @@
+using Mango.GatewaySolution.Extension;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthentication();
+builder.Services.AddOcelot();
+
+
+
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
+app.Run();
