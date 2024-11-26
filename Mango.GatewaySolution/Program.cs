cade2cb672be02c4712b730d51060f9c9bfb694c
BUGMAN-ini <bshaverda@gmail.com>
2024-11-26 23:01:57 +0400

    Done!

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -7,10 +7,6 @@ builder.AddAppAuthentication();
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
-
-
-
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
